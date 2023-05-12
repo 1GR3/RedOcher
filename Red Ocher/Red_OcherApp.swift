50d020dd57c3ba488c1975525b6810fd1817091a
1GR3 <ivo.gregurec@gmail.com>
2023-05-12 11:23:36 +0200

    Initial Commit

---
+++ Red Ocher/Red_OcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Red_OcherApp.swift
+//  Red Ocher
+//
+//  Created by Ivo Gregurec on 12.05.2023..
+//
+
+import SwiftUI
+
+@main
+struct Red_OcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
